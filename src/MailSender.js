529fbb95ea1061963b25d5405c2e0c920b8daa03
suryaagung-Z <suryaagung118@gmail.com>
2025-03-26 00:52:29 +0700

    change env mail sender

--- src/MailSender.js
+++ src/MailSender.js
@@ -3,11 +3,12 @@ const nodemailer = require('nodemailer');
 class MailSender {
   constructor() {
     this._transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
       auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD,
+        user: process.env.MAIL_ADDRESS,
+        pass: process.env.MAIL_PASSWORD,
       },
     });
   }
